04889fad2624a1756573a7d90929a6f3c9e7da6a
PramodSuthar <67489670+PramodSuthar@users.noreply.github.com>
2025-06-15 20:44:06 -0400

    useState and useEffect examples.

--- src/hooks/use-effect/index.jsx
+++ src/hooks/use-effect/index.jsx
@@ -1,4 +1,24 @@
 import { useEffect, useState } from "react";
 
 export const EffectExample = () => {
+
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => setData(json));
+    //console.log('Hey there')
+  },[])
+
+  return (
+    <div>
+      { " " }
+      <h1> Posts</h1>
+      <ul>
+        {data.map((item) => (
+          <li>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
 };
--- src/hooks/use-state/index.jsx
+++ src/hooks/use-state/index.jsx
@@ -2,6 +2,15 @@ import { useState } from "react";
 
 
 export const StateExample = () => {
+  const [count, setCount] = useState(0);
 
+  const increaseCount = () => setCount((prev) => prev + 1);
+
+  return (
+    <div>
+      <p>Counter: {count}</p>
+      <button onClick={increaseCount}>Increase</button>
+    </div>
+  );
 };
 
